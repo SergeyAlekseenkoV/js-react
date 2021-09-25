48c8e74179e19d2aaa282ae47861562f45c5625d
Sergey Alekseenko <alexsvmodest@gmail.com>
2021-09-25 13:16:31 +0300

    arrays,copies

--- main.js
+++ main.js
@@ -1,141 +1,196 @@
-/* Задание на урок:
+"use strict";
 
-1) Первую часть задания повторить по уроку
+// ========= objects ========= //
 
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
-'use strict';
-
-/* ======= task ======= */
-
-// let numberOfFilms;
-
-// function start() {
-//     numberOfFilms = +prompt("How many films have you seen?","");
-//     while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//         numberOfFilms = +prompt("How many films have you seen?","");
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red',
+//     },
+//     makeTest: function() {
+//         console.log('Tested!');
 //     }
-// }
-
-// start();
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false,
 // };
-    
-//     function rememberMyFilms() {
-//         for(let i = 0; i < 2; i++) {
-//             var  a = prompt("Tell us one of the last films",""),
-//                  b = prompt("How are you evaluating it?","");
-    
-//             if(a != null && b != null && a !='' && b != '' && a.length < 50) {
-//                 personalMovieDB.movies[a] = b;
-//                 console.log('done');
-//             } else {
-//                 console.log('error');
-//                 i--;
-//             }
+
+// options.makeTest();
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// console.log(options['colors']['border']);
+
+// ===== Object destructuring ===== //
+
+// const {border, bg} = options.colors;
+// console.log(border);
+
+// ===== loop through the object by using Method for(in) ===== //
+
+// for (let key in options) {
+//     console.log(`Property ${key} has a value ${options[key]}`);
+// }
+
+// for(of) method doesn't work for looping through the object //
+// let counter = 0;
+// for (let key in options) {
+//     if(typeof(options[key]) === 'object'){
+//         for (let i in options[key]) {
+//             console.log(`Property ${i} has a value ${options[key][i]}`);
+//             // counter++;
 //         }
-//     }
-    
-//     rememberMyFilms();
-// //    ======== the same loop by using while loop ======== //
-
-//     // var i = 0;
-//     // while(i < 2) {
-//     //     var  a = prompt("Tell us one of the last films",""),
-//     //          b = prompt("How are you evaluating it?","");
-
-//     //     if(a != null && b != null && a !='' && b != '' && a.length < 50) {
-//     //         personalMovieDB.movies[a] = b;
-//     //         console.log('done');
-//     //     } else {
-//     //         console.log('error');
-//     //         i--;
-//     //     }
-//     //     i++;
-//     // }
-
-// function defineTheLevel(){
-//     if (personalMovieDB.count < 10) {
-//         console.log("You have seen such little amount");
-//     } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
-//         console.log("You are the common watcher");
-//     } else if (personalMovieDB.count >= 30) {
-//         console.log("You are the big fun of films");
 //     } else {
-//         console.log("An error occured!");
+//         console.log(`Property ${key} has a value ${options[key]}`);
+//         counter++;
 //     }
 // }
+// console.log(counter);
 
-// defineTheLevel();
+// ===== arrays ===== //
 
-// function showMyDB(hidden) {
-//     if (!hidden) {
-//         console.log(personalMovieDB);
-//     } 
+// var arr = [2,3,4,5,6];
+
+// last element position
+
+// let last = arr.length -1;
+// console.log(last);
+// console.log(arr.length);
+// ===== callback function gets 3 arguments,firstly forEach goes after callbacks inside running
+// ===== elem - element, i - iterations, arr - link to the array
+// arr.forEach(function(elem, i, arr){
+//    console.log(`${i}: ${elem} inside ${arr}`);
+// });
+
+
+// var strArr = prompt("","");
+// var products = strArr.split(", ");
+// products.sort();
+// console.log(products.join(';'));
+
+// var numArr = [2, 13 , 45 , 453, 222];
+// numArr.sort(compareNum);
+// console.log(numArr);
+
+// // callback function to sort numbers 
+
+// function compareNum(a , b) {
+//     return a - b;
 // }
+// //
+//
+// ===== pass data through value or through the link ===== //
 
-// showMyDB(personalMovieDB.privat);
+let someA = 5,
+    someB = someA; // these work with the ptimitives alone
+someB = someB + 5;
 
-// function writeYourGenres() {
-//     for(var i = 1; i <= 3; i++) {
-//         // const genre = prompt(`Your favorite genre is ${i}`);
-//         // personalMovieDB.genres[i - 1] = genre;
-//         personalMovieDB.genres[i - 1] = prompt(`Your favorite genre is ${i}`);
-//     }
-// }
+console.log(someB);
+console.log(someA);
 
-// writeYourGenres();
+const obj = {
+    someA: 5,
+    someB: 1
+};
 
-var a = [41,2,43,43,444,4,4,4,4,4];
-var rar = [];
+// ==== !!!! ==== passed through the link ==== !!!! ==== ///
+const copy = obj;
 
-function revAr() {
-    
-    for(; a.length; ){
-        var cut = a.pop();
-        rar.push(cut);
+copy.someA = 10;
+
+console.log(copy);
+console.log(obj);
+
+function copyC(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for(key in mainObj) {
+        objCopy[key] = mainObj[key];
     }
-}
-revAr();
-console.log(rar.lenght);
-console.log(rar);
 
-// ==== callback functions ==== //
-
-function someFunction() {
-    // do something
-    setTimeout(function(){
-        console.log(1);
-    },500);
+    return objCopy;
 }
 
-function doSmth(){
-    console.log(2);
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+// ==== we have made the clone(copy),it is shallow copy ==== //
+const newNumbers = copyC(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+var add = {
+    d: 17,
+    e: 20
+};
+// add.d = 20;
+// //  independent shallow copy
+// console.log(Object.assign(numbers, add));
+
+var clone = Object.assign({}, add);
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone); // in clone stores shallow copy of the previuos Obj
+
+// ======= create copy of an Array ========== //
+
+const oldArray = ["a","b","c"];
+const newArray = oldArray.slice();
+
+newArray[1] = "asdasdsa";
+
+console.log(newArray);
+console.log(oldArray);
+
+const videos = ["youtube", "vimeo", "rutube"],
+    blogs = ["wordpress", "livejournal", "blogger"],
+    internet = [...videos, ...blogs, 'vk', 'facebook'];
+
+    console.log(internet);
+
+function log(a,b,c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-someFunction();
-doSmth();
+const num = [2,5,7];
 
-function learnJS(lang, callback) {
-    console.log(`I'am learning ${lang}`);
-    callback();
-}
+log(...num);
 
-function done() {
-    console.log('I made it!');
-}
+// create copy of an array //
+const someArray = ["a", "b"];
 
-learnJS('JS', done);
+const newSomeArray = [...someArray];
+
+console.log(newSomeArray);
+
+// == copy of an Objects == //
+
+const q = {
+    one:1,
+    two: 2
+};
+
+const newObj = {...q};
+
+console.log(newObj);
+
