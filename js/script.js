84de2ad4949a2d525365365a60e9b63df20bd203
Sergey Alekseenko <alexsvmodest@gmail.com>
2021-09-29 04:36:51 +0300

    practice assignment

--- js/script.js
+++ js/script.js
@@ -1,114 +1,56 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
 'use strict';
 
-// ==== getting elements throught the different methods ==== //
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-// const box = document.getElementById("box");
-// console.log(box);
-
-// const btns = document.getElementsByTagName('button');
-// console.log(btns[1]); // get chosen second button
-
-// const circles = document.getElementsByClassName('circle');
-// console.log(circles[2]);
-
-// const hearts = document.querySelectorAll('.heart');
-
-// hearts.forEach(item => {
-//     console.log(item);
-// });
-
-// // returns first element
-// const oneHeart = document.querySelector('.heart');
-// console.log(oneHeart);
-
-// const div = document.querySelector('div');
-// console.log(div); // div #box
+let adv = document.querySelectorAll(".promo__adv img"),
+    poster = document.querySelector(".promo__bg"),
+    genre = poster.querySelector(".promo__genre"),
+    movieList = document.querySelector('.promo__interactive-list');
 
 
-
-// ==== actions with elements on the page ==== //
-
-// ==== changing style ==== //
-
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart'),
-      wrapper = document.querySelector('.wrapper');
-      //oneHeart = wrapper.querySelector('.heart');
-
-btns[1].style.borderRadius = "5em";
-btns[1].style.borderInline = "1px";
-btns[1].style.borderInlineColor = "red";
-btns[1].style.borderInlineStyle = "solid";
-
-circles[1].style.backgroundColor = "yellow";
-
-box.style.cssText = "background-color: lightblue;color: darkblue;width: 50vw;";
-
-// for (let i = 0; i < hearts.length; i++) {
-//     hearts[i].style.backgroundColor = "lightgreen";
-// }
-
-hearts.forEach(item => {
-    item.style.backgroundColor = "darkgreen";
+// remove advertisment   
+adv.forEach(item => {
+    item.remove();
 });
-
-// ==== creating elements dynamically ==== //
-
-const div = document.createElement('div');
-// div.style.cssText = "height: 50px; width: 40vh;background-color: blue;";
-// var text = document.createTextNode('i was here...');
-
-div.classList.add('black');
-// document.body.append(div);
-// document.body.appendChild(div);
-// ==== place element into the end ==== //
-wrapper.append(div);
-// ==== place element to the start ==== //
-wrapper.prepend(div);
-
-// === adding element before === //
-hearts[0].before(div);
-// === old method === //
-// wrapper.insertBefore(div, hearts[0]);
-
-// === adding element after === //
-hearts[0].after(div);
-// === removing of the element .circle === //
-circles[1].remove();
-// === old method === //
-// wrapper.removeChild(hearts[1]);
-
-// === replace element === //
-hearts[0].replaceWith(circles[0]);
-// === old method === //
-// wrapper.replaceChild(circles[0], hearts[0]);
-
-// === adding content dynamically === //
-
-div.innerHTML = '<h4>"Hi there!"</h4>';// whole structure
-
-// div.textContent = "Hello!"; // just content
-
-// insert code before or after
-
-div.insertAdjacentHTML('beforebegin', '<h4>Ho-ho!</h4>');
-
-div.insertAdjacentHTML('afterbegin', '<h4>Ho-ho!</h4>');
-div.insertAdjacentHTML('beforeend', '<h4>Ho-ho!</h4>');
-div.insertAdjacentHTML('afterend', '<h4>Ho-ho!</h4>');
-
-
-
-
-
-
-
-
-
-
+// replace content
+genre.textContent = "драма";
+// changing bg
+poster.style.backgroundImage = "url('img/bg.jpg')";
+// clear the static data from html layout
+movieList.innerHTML = "";
+console.log(poster.innerHTML);
+// ==== sort elements ==== //
+movieDB.movies.sort();
+// go through them 
+movieDB.movies.forEach(function(film, i){
+    movieList.innerHTML +=  `
+        <li class="promo__interactive-item">${i + 1}&nbsp;${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
 
 
 
