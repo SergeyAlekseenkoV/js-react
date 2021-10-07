72e5a06a936bb4629e22192c9adf668d6ca69a6c
Sergey Alekseenko <alexsvmodest@gmail.com>
2021-10-07 05:38:57 +0300

    date()&time,benchmark

--- js/script.js
+++ js/script.js
@@ -1,61 +1,36 @@
-// =========== managing the time of scripts executing ========== //
-// setTimeout() and setInterval
-const btn = document.querySelector('.btn');
-// const timerId = setTimeout(function(text) {
-//     console.log(text);
-// }, 2000, "Hi there man!");
+"use strict";
 
-let timerN,
-    i = 0;
+// =========== work with Date() ============ //
 
+let now = new Date();
 
-function myAnime() {
-    const element = document.querySelector('.box');
-    let position = 0;
+// current date and time
+// console.log(now);
 
-    // ===> start interval
-    const id = setInterval(frame, 20);
-    
-    // each movement
-    function frame() {
-        if(position == 300) {
-            clearInterval(id);
-        } else if(position == 150) {
-            position++;
-            element.style.bottom = position*0 +'px';
-            element.style.right = position*0 +'px';
-        } else { // if our animation haven't stopped
-            position++;
-            element.style.top = position + 'px';
-            element.style.left = position + 'px';
-        }
-    }
-}// we need to start interval up above the frame function ===>
+// now = new Date(2021, 5, 1, 20); 
+// console.log(now);
 
-// assign the trigger of the animation
-btn.addEventListener('click', myAnime);
+// since 1970 all dates goes in milliseconds
+now = new Date(); 
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
 
-// stop executing the timer
-/* clearInterval(timerId);
+// console.log(now.getTime());
+// console.log(now.getTimezoneOffset());
 
-const Timer = setTimeout(logger, 1000);
+console.log(now.setHours(18));
 
-function logger() {
-    if (i === 3) {
-        clearInterval(timerN);
-    } else {
-        clearInterval();
-    }
+// benchmark
 
-    console.log('TEXT');
-    i++;
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
 }
 
-btn.addEventListener('click', () => {
-    timerN = setInterval(logger, 500);
-});
-
-let id = setTimeout(function log(){
-    console.log('hey man!');
-    id = setTimeout(log, 500);
-}, 500); */
+let end = new Date();
+console.log(`loop done after ${end -start}ms`);
